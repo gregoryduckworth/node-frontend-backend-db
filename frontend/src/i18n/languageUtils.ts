a41cf7f5523f076d720826bc6c17d81c9db2c113
Greg Duckworth <gregoryduckworth@googlemail.com>
2025-05-04 11:21:30 +0100

    translations contain language codes

---
+++ frontend/src/i18n/languageUtils.ts
@@ -0,0 +1,29 @@
+import { TFunction } from 'i18next';
+
+export interface Language {
+  code: string;
+  nativeName: string;
+}
+
+export const getAvailableLanguages = (t: TFunction, languageCodes: string[]): Language[] => {
+  return languageCodes.map((code) => ({
+    code,
+    nativeName: t(`languages.${code}`),
+  }));
+};
+
+export const getCurrentLanguage = (
+  t: TFunction,
+  currentLanguageCode: string,
+  availableLanguages: Language[]
+): Language => {
+  const code = currentLanguageCode.split('-')[0];
+  const currentLanguage = availableLanguages.find((lang) => lang.code === code);
+
+  return (
+    currentLanguage || {
+      code,
+      nativeName: t(`languages.${code}`, code),
+    }
+  );
+};
