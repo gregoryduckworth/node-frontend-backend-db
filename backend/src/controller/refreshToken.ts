3aa5357f2870a1a073c78228bfeacab9e6c17a16
Greg Duckworth <gregoryduckworth@googlemail.com>
2025-05-04 21:48:26 +0100

    expand tests

--- backend/src/controller/refreshToken.ts
+++ backend/src/controller/refreshToken.ts
@@ -2,18 +2,36 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { prisma } from "@prismaClient/client";
 
+export const generateAccessToken = (user: any, secret: string) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      dateOfBirth: user.dateOfBirth ? user.dateOfBirth.toISOString() : null,
+    },
+    secret,
+    { expiresIn: "30m" }
+  );
+};
+
 export const refreshToken = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   try {
     const refreshToken = req.cookies.refreshToken;
-    if (!refreshToken) return res.sendStatus(204);
+    if (!refreshToken) {
+      return res.sendStatus(204);
+    }
 
     const user = await prisma.user.findFirst({
       where: { refresh_token: refreshToken },
     });
-    if (!user) return res.sendStatus(403);
+    if (!user) {
+      return res.sendStatus(403);
+    }
 
     const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
     const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
@@ -26,24 +44,14 @@ export const refreshToken = async (
     try {
       jwt.verify(refreshToken, refreshTokenSecret);
     } catch (err) {
+      console.error("JWT verification failed:", err);
       return res.sendStatus(403);
     }
 
-    const accessToken = jwt.sign(
-      {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        dateOfBirth: user.dateOfBirth ? user.dateOfBirth.toISOString() : null,
-      },
-      accessTokenSecret,
-      { expiresIn: "30m" }
-    );
-
+    const accessToken = generateAccessToken(user, accessTokenSecret);
     return res.json({ accessToken });
   } catch (error) {
-    console.log(error);
+    console.error("Error refreshing token:", error);
     return res.sendStatus(500);
   }
 };
