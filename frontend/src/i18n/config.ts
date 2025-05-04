6be1c707279dbb24c3064d50f24caad46d0f3c29
Greg Duckworth <gregoryduckworth@googlemail.com>
2025-05-04 22:51:25 +0100

    add french

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -4,6 +4,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationEN from './locales/en/translation.json';
 import translationES from './locales/es/translation.json';
+import translationFR from './locales/fr/translation.json';
 
 const resources = {
   en: {
@@ -11,6 +12,9 @@ const resources = {
   },
   es: {
     translation: translationES,
+  },
+  fr: {
+    translation: translationFR,
   },
 };
 
