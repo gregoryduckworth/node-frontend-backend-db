5eb9bd75059c0d3d73863086944e906b0917b565
Greg Duckworth <gregoryduckworth@googlemail.com>
2025-05-04 21:10:20 +0100

    jest + supertest for backend

---
+++ backend/src/app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import UserRoute from "@routes/UserRoute";
+
+dotenv.config();
+
+const app = express();
+
+const allowedOrigins = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(",")
+  : ["http://localhost:5173"];
+
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/health", (_req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+app.use(UserRoute);
+
+export default app;
