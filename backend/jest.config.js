8ee8fb77ab618e1f607f9534fa0a81f8d7e0993d
Greg Duckworth <gregoryduckworth@googlemail.com>
2025-05-04 21:10:20 +0100

    jest + supertest for backend

---
+++ backend/jest.config.js
@@ -0,0 +1,15 @@
+// jest.config.js
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@controller/(.*)$": "<rootDir>/src/controller/$1",
+    "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
+    "^@routes/(.*)$": "<rootDir>/src/routes/$1",
+    "^@types/(.*)$": "<rootDir>/src/types/$1",
+    "^@prismaClient/(.*)$": "<rootDir>/prisma/$1",
+  },
+  setupFiles: ["dotenv/config"],
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,5 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import UserRoute from "@routes/UserRoute";
+import app from './app';
 
-// Load environment variables
-dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = process.env.CORS_ORIGINS
-  ? process.env.CORS_ORIGINS.split(",")
-  : ["http://localhost:5173"];
-
-const corsOptions = {
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-// Middleware setup
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
-
-// Health check endpoint
-app.use("/health", (_req: Request, res: Response) => {
-  res.sendStatus(200);
-});
-
-// Routes setup
-app.use(UserRoute);
-
-// Start server
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
