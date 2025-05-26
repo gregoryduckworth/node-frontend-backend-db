6e75a707cb3ed107a4a7c84be5ec86587a9c7921
Greg Duckworth <gregoryduckworth@googlemail.com>
2025-05-26 21:56:47 +0100

    move over to yarn

--- admin-frontend/src/main.tsx
+++ admin-frontend/src/main.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 import '@i18n/config';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
